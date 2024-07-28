9673a187f5d897b23a78120a671cc97343add62d
Babita Chaturvedi <babita.chaturvedi@seceon.com>
2024-07-28 18:25:27 +0530

    Initial Commit

---
+++ src/app/services/seller.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { SellerComponent } from '../seller/seller.component';
+import{HttpClient} from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SellerService {
+
+  constructor( HttpClient : HttpClient ) { }
+
+ usersignup(){
+   
+ }
+
+
+}
